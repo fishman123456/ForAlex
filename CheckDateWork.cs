23fa37ce677e6bb81f2d729ab06479c099583d80
Denis Borisovich Fishman <106389581+fishman123456@users.noreply.github.com>
2023-12-18 16:45:27 +0700

    Initial commit

---
+++ CheckDateWork.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ACADCommands
+{
+    // класс для проверки текущей даты
+    public static class CheckDateWork
+    {
+        public static void CheckDate()
+        {
+            DateTime dt1 = DateTime.Now;
+            DateTime dt2 = DateTime.Parse("01/01/2024");
+            Window w1 = new Window();
+
+            if (dt1.Date > dt2.Date)
+            {
+                //MessageBox.Show("Your Application is Expire");
+                // Выход из проложения добавил 01-01-2024. Чтобы порядок был....
+                Application.Current.Shutdown();
+                //w1.Close();
+            }
+            else
+            {
+                //MessageBox.Show("Работайте до   " + dt2.ToString());
+            }
+
+        }
+    }
+}
---
+++ Class1.cs
@@ -0,0 +1,89 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+using System.Text;
+
+// Эта строка не является обязательной, но улучшает производительность загрузки
+[assembly: CommandClass(typeof(ACADCommands.GetAttribValue))]
+
+namespace ACADCommands
+{
+    public class GetAttribValue
+    {
+        [CommandMethod("LISTATT")]
+
+
+
+        public static void ListAttributes()
+        {
+            CheckDateWork.CheckDate();
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            Database db = HostApplicationServices.WorkingDatabase;
+
+            Transaction tr = db.TransactionManager.StartTransaction();
+            // Start the transaction
+            try
+            {
+                // Build a filter list so that only
+                // block references are selected
+                TypedValue[] filList = new TypedValue[1] { new TypedValue((int)DxfCode.Start, "INSERT") };
+                SelectionFilter filter = new SelectionFilter(filList);
+                PromptSelectionOptions opts = new PromptSelectionOptions();
+                opts.MessageForAdding = "Select block references: ";
+                PromptSelectionResult res = ed.GetSelection(opts, filter);
+                // Do nothing if selection is unsuccessful
+                if (res.Status != PromptStatus.OK)
+                    return;
+                SelectionSet selSet = res.Value;
+                ObjectId[] idArray = selSet.GetObjectIds();
+                StringBuilder stringBuilder = new StringBuilder();
+
+                foreach (ObjectId blkId in idArray)
+                {
+                    BlockReference blkRef = (BlockReference)tr.GetObject(blkId, OpenMode.ForRead);
+
+                    BlockTableRecord btr = (BlockTableRecord)tr.GetObject(blkRef.BlockTableRecord, OpenMode.ForRead);
+
+                    // btr.Dispose();
+                    AttributeCollection attCol = blkRef.AttributeCollection;
+                    //  выводим координаты блока,слой и handle
+
+                    string str = ("\n--------------------------\n" +
+                                       "Block name: " + btr.Name + ",\n" +
+                                       "ID: " + blkRef.Id.ToString() + ",\n" +
+                                       "X: " + blkRef.Position.X.ToString() + ",\n" +
+                                       "Y: " + blkRef.Position.Y.ToString() + ",\n" +
+                                       "Z: " + blkRef.Position.Z.ToString() + ",\n" +
+                                       "Handle BlockRef : " + blkRef.Handle.ToString() + ",\n" + // вот нужная фигня - Handle
+                                       "Layer: " + blkRef.Layer.ToString() + ",\n");
+                    stringBuilder.Append(str);
+                    ed.WriteMessage(str);
+                    foreach (ObjectId attId in attCol)
+                    {
+                        AttributeReference attRef = (AttributeReference)tr.GetObject(attId, OpenMode.ForRead);
+                        // добавил условие по проверке название тега  ОБОЗНАЧ_КАБЕЛЯ
+                        if (attRef.Tag == "ОБОЗНАЧ_КАБЕЛЯ")
+                        {
+                            str = ("Attribute Tag: " + attRef.Tag + ",\n" +
+                                "Attribute String: " + attRef.TextString + ",\n");
+                            ed.WriteMessage(str);
+                        }
+                    }
+                }
+                tr.Commit();
+            }
+            catch (Autodesk.AutoCAD.Runtime.Exception ex)
+            {
+                ed.WriteMessage(("Exception: " + ex.Message));
+            }
+            finally
+            {
+                tr.Dispose();
+            }
+        }
+    }
+}
+
